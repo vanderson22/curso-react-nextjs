4b507c8c7e7358079c99b46e9e53b924e413ca06
Your Name <you@example.com>
2024-08-06 15:49:17 -0300

    css module

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,21 @@
+
+import estilos from "./integracao2.module.css"
+//??styles
+export default function integracao2() {
+
+
+    return (
+
+        <>
+            <div id={estilos.integracao2}>
+                <div className={estilos.vermelho}>
+                    Texto #01
+                </div>
+                <div className={estilos.azul}>
+                    Texto #02
+                </div>
+
+            </div>
+        </>
+    )
+}
