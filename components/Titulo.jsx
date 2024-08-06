76a9e95f13f7162c03ce6170a2b0f91f16c36253
Your Name <you@example.com>
2024-08-06 13:33:02 -0300

    utilizando componentes

---
+++ components/Titulo.jsx
@@ -0,0 +1,11 @@
+//Maiusculo pois não é uma URL.
+export default function Titulo(){
+
+    return (
+        <>
+         <h1>Titulo</h1>
+         <h2>subTítulo</h2>
+        </>
+    )
+
+}
---
+++ pages/basicos/usando-titulo.jsx
@@ -0,0 +1,11 @@
+import Titulo from "../../components/Titulo";
+//atenção ao import.
+export default function usandoTitulo(){
+
+    //sistema de módulo.
+    return (
+        <div>
+            <Titulo />
+        </div>
+     )
+} 
